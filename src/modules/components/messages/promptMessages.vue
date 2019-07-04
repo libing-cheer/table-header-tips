15ed629232d40f5bfc59ddeb7b4fa9940e9ad640
libing-cheer <libing_cheer@163.com>
2019-07-04 09:45:31 +0800

    提示组件promptMessages修改

--- src/modules/components/messages/promptMessages.vue
+++ src/modules/components/messages/promptMessages.vue
@@ -5,7 +5,7 @@
         <!-- 插槽，可提供多行的提示信息 -->
         <!-- 全局组件，这里是配置图标icon和提示信息的地方 -->
         <!-- 在其他组件中引用方式详见 table-header-tips.vue 组件中的 renderHeaderMethods 方法 -->
-        <span v-for="(item, index) in messages" :key="index">{{item}}</span>
+        <p v-for="(item, index) in messages" :key="index">{{item}}</p>
       </div>
       <i class="el-icon-info" style="color:#409eff;margin-left:5px;"></i>
     </el-tooltip>
